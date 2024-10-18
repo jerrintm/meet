b96dac234a1341443e80b8a814ae563d98b001ee
jerrintm <jerrintm@gmail.com>
2024-10-17 22:16:14 -0400

    Changes for Task 4.4

--- src/__tests__/App.tests.js
+++ src/__tests__/App.tests.js
@@ -4,8 +4,65 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('<App /> component', () => {
+    let AppDOM;
+  beforeEach(() => {
+    AppDOM = render(<App />).container.firstChild;
+  })
+    
+    
     test('renders list of events', () => {
         const AppDOM = render(<App />).container.firstChild;
         expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+    });
+
+    test('render CitySearch', () => {
+        const AppDOM = render().container.firstChild;
+        expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+        });
+
+    test('render NumberOfEvents', () => {
+        expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
+        });
+});
+
+describe('<App /> integration', () => {
+  
+    test('renders a list of events matching the city selected by the user', async () => {
+      const user = userEvent.setup();
+      const AppComponent = render(<App />);
+      const AppDOM = AppComponent.container.firstChild;
+  
+      const CitySearchDOM = AppDOM.querySelector('#city-search');
+      const CitySearchInput = within(CitySearchDOM).queryByRole('textbox');
+  
+      await user.type(CitySearchInput, "Berlin");
+      const berlinSuggestionItem = within(CitySearchDOM).queryByText('Berlin, Germany');
+      await user.click(berlinSuggestionItem);
+  
+      const EventListDOM = AppDOM.querySelector('#event-list');
+      const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');
+  
+      const allEvents = await getEvents();
+      const berlinEvents = allEvents.filter(event => event.location === 'Berlin, Germany');
+      expect(allRenderedEventItems.length).toBe(berlinEvents.length);
+      
+      allRenderedEventItems.forEach(event => {
+        expect(event.textContent).toContain("Berlin, Germany");
+      });
+    });
+    
+    test('updates the number of events displayed when the user changes the number of events input', async () => {
+      const user = userEvent.setup();
+      const AppComponent = render(<App />);
+      const AppDOM = AppComponent.container.firstChild;
+  
+      const NumberOfEventsDOM = AppDOM.querySelector('#number-of-events');
+      const NumberOfEventsInput = within(NumberOfEventsDOM).queryByTestId('numberOfEventsInput');
+  
+      await user.type(NumberOfEventsInput, '{backspace}{backspace}10');
+  
+      const EventListDOM = AppDOM.querySelector('#event-list');
+      const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(allRenderedEventItems.length).toBe(10);
     });
 });
