a00fb277e7575045c70f3956f796b74a686449ea
jerrintm <jerrintm@gmail.com>
2024-11-01 23:34:13 -0400

    Updates for Task 4.8

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -2,10 +2,20 @@
 
 import { useEffect, useState } from "react";
 
-const CitySearch = ({ allLocations, setCurrentCity }) => {
+const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+
+
+  const handleItemClicked = (event) => {
+    const value = event.target.textContent;
+    setQuery(value);
+    setShowSuggestions(false); // to hide the list
+    setCurrentCity(value); 
+    setInfoAlert('');
+  };
+
 
   const handleInputChanged = (event) => {
     const value = event.target.value;
@@ -14,14 +24,17 @@ const CitySearch = ({ allLocations, setCurrentCity }) => {
     }) : [];
     setQuery(value);
     setSuggestions(filteredLocations);
+  
+    let infoText;
+    if (filteredLocations.length === 0) {
+      infoText = 'We can not find the city you are looking for. Please try another city';
+    } else {
+      infoText = '';
+    }
+    setInfoAlert(infoText);
   };
 
-  const handleItemClicked = (event) => {
-    const value = event.target.textContent;
-    setQuery(value);
-    setShowSuggestions(false); // to hide the list
-    setCurrentCity(value); 
-  };
+  
   
   useEffect(() => {
     setSuggestions(allLocations);
@@ -29,6 +42,8 @@ const CitySearch = ({ allLocations, setCurrentCity }) => {
 
   return (
       <div id="city-search">
+        <label htmlFor="city" id='city-label'>
+        Search City:
         <input 
           type="text"
           className="city"
@@ -36,18 +51,19 @@ const CitySearch = ({ allLocations, setCurrentCity }) => {
           value={query}
           onFocus={() => setShowSuggestions(true)}
           onChange={handleInputChanged}
+          onInput={() => (
+            query.length === 0 ? setShowSuggestions(false) : setShowSuggestions(true)
+          )}   
         />
-        {showSuggestions ? 
-          <ul className="suggestions">
+        </label>
+        {showSuggestions ? <ul className="suggestions">
             {suggestions.map((suggestion) => {
               return <li 
-                        onClick={handleItemClicked} 
-                        key={suggestion}>{suggestion}
+                        key={suggestion}  onClick={handleItemClicked}>{suggestion}
                       </li>
             })}
             <li key='See all cities'
-                onClick={handleItemClicked}
-            >
+                onClick={handleItemClicked}  >
               <b>See all cities</b>
             </li>
           </ul> : null}
